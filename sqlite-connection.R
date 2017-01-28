c1456e09a56d4d2f8bf67d1be97a7c3b3b04b135
ianfrantz <ian@ianfrantz.com>
2017-01-27 19:26:02 -0800

    New core files

--- sqlite-connection.R
+++ sqlite-connection.R
@@ -1,3 +1,16 @@
 #Build connection to SQLite database 
 
 library(RSQLite)
+
+setwd ("C:/Users/ianfr/Desktop/R Programs/forecaster")
+
+#Connect to SQLite forecaster.db
+forecaster.db <- dbConnect(SQLite(),dbname="forecaster.db")
+
+
+#Extract Data from forecaster.db
+hierarchy <- dbGetQuery(forecaster.db, "SELECT * FROM product")
+
+#dbList examples:
+dbListTables(forecaster.db) #All tables
+dbListFields(forecaster.db, "product") #Fields in a table
