ef7263518849e6c881a15c08634319e5e6ed3d2c
ianfrantz <ian@ianfrantz.com>
2017-06-02 20:20:21 -0700

    Closed SQLite connection and cleaned up comments

--- sqlite-connection.R
+++ sqlite-connection.R
@@ -8,16 +8,13 @@ setwd ("C:/Users/ianfr/Desktop/R Programs/forecaster")
 forecaster.db <- dbConnect(SQLite(),dbname="forecaster.db")
 
 
-#Extract Data from forecaster.db
+#Create hierarchy and producttable dataframes from tables in forecaster.db
 hierarchy <- dbGetQuery(forecaster.db, "SELECT * FROM coreproducts")
-
-#dbList examples:
-dbListTables(forecaster.db) #All tables
-
 
 producttable <- dbGetQuery(forecaster.db, 
           "SELECT product_name, tier_name, offer_number, price, probability
            FROM coreproducts JOIN pricing USING (product_id)
            ORDER BY product_name, tier_name;")
 
-
+#Close database connection
+dbDisconnect(forecaster.db)
