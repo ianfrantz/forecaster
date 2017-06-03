c24c9bb2d518b46f0c64c6497528a37270feafb7
ianfrantz <ian@ianfrantz.com>
2017-06-02 21:17:36 -0700

    Added return to function and re-ordered code

--- simulator.R
+++ simulator.R
@@ -1,9 +1,19 @@
 library (dplyr)
 library (purrr)
 
-#Create P1T1 dataframe with only "Product 1" and "Tier 1" data
+#Define simulator function
 
-#Need to write a function here
+simulator <- function(samplesize, price, probability) {
+  output <- c ()
+  for (i in 1:52)
+  {
+    simulation <- pmap(list(x = price, size = samplesize, replace = TRUE, prob = probability), sample)
+    output <- append(output, sum(simulation[[1]]))
+  }
+  return(output)
+}
+
+#Create product dataframes with only "Product 1" and "Tier 1" data
 
 P1T1 <- filter (producttable, product_name == "Product 1") %>% 
   filter (tier_name == "Tier 1")
@@ -15,18 +25,5 @@ P1T2 <- filter (producttable, product_name == "Product 1") %>%
 
 p1t1price <- list(P1T1$price)
 p1t1probability <- list (P1T1$probability)
-
-#Define simulator function
-
-simulator <- function(samplesize, price, probability) {
-  output <- c ()
-  for (i in 1:52)
-  {
-    simulation <- pmap(list(x = price, size = samplesize, replace = TRUE, prob = probability), sample)
-    output <- append(output, sum(simulation[[1]]))
-  }
-  output
-}
-
 
 
