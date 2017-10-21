d2825f4b60d7b98cc08fc6c96b85dff36a154eaa
ianfrantz <ian@ianfrantz.com>
2017-10-21 11:30:32 -0700

    Updated file locations and setwd in R code

--- sqlite-connection.R
+++ sqlite-connection.R
@@ -2,11 +2,10 @@
 library(RSQLite)
 library(dplyr)
 
-setwd ("C:/Users/ianfr/Desktop/R Programs/forecaster")
+setwd ("C:/Users/ianfr/Desktop/R_Programs/forecaster/Database")
 
 #Connect to SQLite forecaster.db
 forecaster.db <- dbConnect(SQLite(),dbname="forecaster.db")
-
 
 #Create hierarchy and producttable data.frames from tables in forecaster.db
 hierarchy <- dbGetQuery(forecaster.db, "SELECT * FROM coreproducts")
@@ -16,11 +15,12 @@ product.table <- dbGetQuery(forecaster.db,
            FROM coreproducts JOIN pricing USING (product_id)
            ORDER BY product_name, tier_name;")
 
-#Save product.table
-save (product.table, file = "product.table.RData")
-
 #Close database connection
 dbDisconnect(forecaster.db)
+
+#Save product.table
+setwd ("C:/Users/ianfr/Desktop/R_Programs/forecaster/Data")
+save (product.table, file = "product.table.RData")
 
 #Build a "Product 1" and "Tier 1" list
 #Create this as a function that takes input for name and tier.
