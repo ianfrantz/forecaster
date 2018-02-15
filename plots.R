9ccf477291c80c2eaf63dd18820e05bce39950fc
ianfrantz <ian@ianfrantz.com>
2018-02-14 16:57:56 -0800

    Working Sales Sheet Answering Business Questions

--- plots.R
+++ plots.R
@@ -8,40 +8,52 @@ x <- 1:lx
 #trial1 results
 trial1 <- data.frame(x,y)
 
-productplot1 <- ggplot(data = trial1) 
+trial1plot <- ggplot(data = trial1) 
 
 #--FUTURE--Sum trial1$y and put results into labels
-productplot1 + 
-  geom_bar(aes(x = y), fill = "green")
+trial1plot + 
+  geom_bar(aes(x = y), fill = "green") + 
+  labs(
+    title = "Title: Fuel efficiency generally decreases with engine size",
+    subtitle = "Subtitle: Two seaters (sports cars) are an exception because of their light weight",
+    caption = "Caption: Data from fueleconomy.gov"
+  )
 
-productplot1 + 
+trial1plot + 
   geom_bar(aes(x = y, y = ..prop..), fill = "green") 
 
 
 #trial #2 (52 weeks, 5 sales per week)
 y <- Simulator(52, p1t1[4], 5, p1t1[5])
+lx <- length(y)
+x <- 1:lx
 trial2 <- data.frame(x,y)
+
+trial2plot <- ggplot(data = trial2) 
+
+trial2plot + 
+  geom_bar(aes(x = y), fill = "red")
 
 
 #Think about how to color the offer numbers into this view. I think it will be more useful with showing the proportionality.
 
-trialresults <-ggplot() + 
+combineresults <- ggplot() + 
   geom_bar(data=trial1, aes(y), fill="green") + 
   geom_bar(data=trial2, aes(y), fill="red")
 
-trialresults
+combineresults
 
 #Plot Densities for trial1 and trial2
 require(reshape2)
 df.m <- melt(data.frame(trial1 = trial1, trial2 = trial2))
 
-productplot1 <- ggplot(df.m) +
+trial1 <- ggplot(df.m) +
   geom_density(aes(x = value, colour = variable)) + 
   labs(x = NULL)
 
-productplot1
+trial1
 
-productplot1 <- ggplot(data = c(trial1, trial2)) + 
+trial1 <- ggplot(data = c(trial1, trial2)) + 
   geom_point(mapping = aes(x=x, y=y, color = "red"))
 
 
