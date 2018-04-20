b8adfe7043bdf4a84bfadb7609588b209adcaded
ianfrantz <ian@ianfrantz.com>
2018-04-20 16:44:32 -0700

    shiny-forecaster.R added with starter code

---
+++ shiny-forecaster.R
@@ -0,0 +1,55 @@
+# Needed libraries for forecaster
+library(shiny)
+library(ggplot2)
+
+#Load data from sqlite-connection.R script
+load("./Data/product.table.RData")
+
+# Define UI for forecaster
+ui <- fluidPage(
+  
+  titlePanel("Forecasting Hierarchical Sales"),
+  # Sidebar layout with a input and output definitions
+  sidebarLayout(
+    
+    # Inputs
+    sidebarPanel(
+      
+      # Select variable for y-axis
+      selectInput(inputId = "y", 
+                  label = "Y-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"), 
+                  selected = "audience_score"),
+      
+      # Select variable for x-axis
+      selectInput(inputId = "x", 
+                  label = "X-axis:",
+                  choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"), 
+                  selected = "critics_score"),
+      
+      # Set alpha level
+      sliderInput(inputId = "alpha", 
+                  label = "Alpha:", 
+                  min = 0, max = 1, 
+                  value = 0.5)
+    ),
+    
+    # Outputs
+    mainPanel(
+      plotOutput(outputId = "scatterplot")
+    )
+  )
+)
+
+# Define server function required to create the scatterplot
+server <- function(input, output) {
+  
+  # Create scatterplot object the plotOutput function is expecting
+  output$scatterplot <- renderPlot({
+    ggplot(data = product.table, aes_string(x = input$x, y = input$y)) +
+      geom_point(alpha = input$alpha)
+  })
+}
+
+# Create the Shiny app object
+shinyApp(ui = ui, server = server)
