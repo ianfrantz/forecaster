ee50f4988393a89a375ac2be19e21a8e3a8bb8e7
ianfrantz <ian@ianfrantz.com>
2018-05-27 18:23:09 -0700

    documenation updates

--- simulator.R
+++ simulator.R
@@ -1,10 +1,14 @@
-#library (purrr) #For use of `pmap`` in the Simulator function
+#-----library (purrr) #For use of `pmap`` in the Simulator function-----
 library (dplyr) #Use of dplyr package in ProductList function
 
 
 #-----Functions "Simulator" and "ProductList"------
 
-#The "Simulator" function is defined here with four variables. 
+#The "Simulator" function is defined here taking four variables. 
+  #-weeks: the number of weeks
+  #-price: product hierarchical pricing
+  #-samplesize: the number of sampling distributions
+  #-probability: pdf - density function
 Simulator <- function(weeks, price, samplesize, probability) {
   output <- c ()
   for (i in 1:weeks)
@@ -20,6 +24,4 @@ Simulator <- function(weeks, price, samplesize, probability) {
 ProductList <- function(product.table, product_name1, tier_name1) {
   dplyr::filter (product.table, product_name1 == product_name, 
           tier_name1 == tier_name) %>% as.list() }
-
-
 
